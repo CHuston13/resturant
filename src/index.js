e28a619e400d67fabfccdebfc3facf40cf191ce9
Chuston13 <cdhprogrammer@gmail.com>
2022-07-29 17:02:00 -0500

    index.html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
+
+
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
+   component=document.getElementById('content');
+   const element = document.createElement('div');
+   const btn = document.createElement('button');
+   element.innerHTML = ("hello" , " ");
+   return element
+
   
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-
-  btn.onclick = printMe;
-
-
-  element.appendChild(btn);
-  
-    return element;
   }
   
   document.body.appendChild(component());
