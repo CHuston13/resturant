5b17289e22167324a9a575fdd6247e75035ecae8
Chuston13 <cdhprogrammer@gmail.com>
2022-07-31 17:41:14 -0500

    index.html

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import _ from 'lodash';
 
 
+
+
+
  function component() {
   const element = document.createElement('button');
   let e = document.createTextNode('hey');
   element.appendChild(e)
+ element.style.color="red"
+ return element;
+}
 
-   
+
   
    
 
 
   // Lodash, now imported by this script
   
-   return element  ;
- }
+ 
 const currentDiv = document.getElementById('content')
  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
+ module.exports = {
+   entry: './src/index.js',
+   output: {
+
+   
+
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+     path: path.resolve(__dirname, 'dist'),
+   },
+
+   module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
   },
 };
