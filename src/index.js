08bb08a9d52b8f97823c21774d13a7796023eacb
Chuston13 <cdhprogrammer@gmail.com>
2022-08-03 10:16:49 -0500

    index.html

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ import './style.css';
    const element = document.createElement('div');
    const b = document.createElement('button');
    const t = document.createTextNode('Home');
-   element.appendChild(b)
+   const h =  document.createElement('h1');
+   const ht = document.createTextNode('Huston Soul Food');
+   element.appendChild(b);
    b.appendChild(t);
+   element.appendChild(h);
+   h.appendChild(ht);
 
    // Lodash, now imported by this script
 
-  element.classList.add('hello','*','html')
-  b.classList.add('good')
+  element.classList.add('hello','*','html');
+  b.classList.add('good');
+  h.classList.add('gooda');
   
   
    return element;
